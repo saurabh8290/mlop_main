4266dd82f5f28dfbf4e29d802924821772db983f
Saurabh Agarwal <saurabh.agarwal@vaibhavglobal.in>
2023-03-30 13:54:11 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Its a wafer Project using MLOps.',
+    author='Saurabh8290',
+    license='MIT',
+)
